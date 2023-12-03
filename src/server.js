4d67091285a1e2fdb2100e19add409a3881ec419
michaelkasingye <michaelkasingye@gmail.com>
2023-12-03 14:12:12 +0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,81 @@
+/*
+  imports
+*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+// 3000 - staging, 2000 - production
+const PORT = process.env.PORT || 2000;
+const knex = require('./db/knex');
+
+/*
+  middleware
+*/
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*
+  routes
+*/
+
+app.get('/', (req, res) => {
+  res.json('pong!');
+});
+
+app.get('/health', (req, res) => {
+  res.json('This server is healthy - test -v0.123');
+});
+
+app.get('/todos', (req, res) => {
+  knex('todos')
+  .then((data) => { res.json(data); })
+  .catch(() => { res.json('Something went wrong.') });
+});
+
+app.get('/todos/:id', (req, res) => {
+  knex('todos')
+  .where({ id:  parseInt(req.params.id) })
+  .then((data) => { res.json(data); })
+  .catch(() => { res.json('Something went wrong.') });
+});
+
+app.post('/todos', (req, res) => {
+  knex('todos')
+  .insert({
+    title: req.body.title,
+    completed: false
+  })
+  .then(() => { res.json('Todo added!'); })
+  .catch(() => { res.json('Something went wrong.') });
+});
+
+app.put('/todos/:id', (req, res) => {
+  knex('todos')
+  .where({ id: parseInt(req.params.id) })
+  .update({
+    title: req.body.title,
+    completed: req.body.completed
+  })
+  .then(() => { res.json('Todo updated!'); })
+  .catch(() => { res.json('Something went wrong.') });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  knex('todos')
+  .where({ id: parseInt(req.params.id) })
+  .del()
+  .then(() => { res.json('Todo deleted!'); })
+  .catch(() => { res.json('Something went wrong.') });
+});
+
+
+/*
+  run server
+*/
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
